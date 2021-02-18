49fa00a1a6308a4f6da388019a742eb4779145ef
pinocchio22 <vviian.2@gmail.com>
2021-02-18 23:06:27 +0900

    feat : MainActivity  composed
    1. Insert Data
    2. Get Data

--- app/src/main/java/com/example/memo/MainActivity.kt
+++ app/src/main/java/com/example/memo/MainActivity.kt
@@ -1,11 +1,73 @@
 package com.example.memo
 
+import android.annotation.SuppressLint
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+@SuppressLint("StaticFieldLeak")
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var db : MemoDatabase
+    var memoList = listOf<MemoEntity>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        db = MemoDatabase.getInstance(this)!!
+
+        button_add.setOnClickListener{
+            val memo = MemoEntity(null, edittext_memo.text.toString())
+            insertMemo(memo)
+        }
+    }
+
+
+    //1. Insert Data
+    //2. Get Data
+    //3. Delete Data
+    //4. Set RecyclerView
+
+    fun insertMemo(memo : MemoEntity){
+        //1. MainThread vs WorkerThread(Background Thread)
+
+        val insertTask = object : AsyncTask<Unit,Unit,Unit>(){
+            override fun doInBackground(vararg params: Unit?) {
+                db.memoDAO().insert(memo)
+            }
+
+            override fun onPostExecute(result: Unit?) {
+                super.onPostExecute(result)
+                getAllMemo()
+            }
+
+        }
+
+        insertTask.execute()
+    }
+
+    fun getAllMemo(){
+        val getTask = (object : AsyncTask<Unit,Unit,Unit>(){
+            override fun doInBackground(vararg params: Unit?) {
+                memoList = db.memoDAO().getAll()
+            }
+
+            override fun onPostExecute(result: Unit?) {
+                super.onPostExecute(result)
+                setRecyclerView(memoList)
+            }
+        }).execute()
+
+    }
+
+    fun deleteMemo(){
+
+    }
+
+    fun setRecyclerView(memoList: List<MemoEntity>){
+
     }
 }
